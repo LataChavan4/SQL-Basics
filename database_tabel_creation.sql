9e3fb73003718a3f727918db2025b7b4e00450d9
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-10-18 23:56:46 +0530

    Create database_tabel_creation.sql

---
+++ database_tabel_creation.sql
@@ -0,0 +1,11 @@
+--database creation
+CREATE DATABASE database_name;
+
+--creating table in database
+create table table_name
+(
+id integer,
+incident_date date,
+name varchar(100),
+price decimal(6,2),
+);
