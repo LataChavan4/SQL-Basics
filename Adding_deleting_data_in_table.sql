22cbed24881f236ded492c6b87ae21212440ebe8
LataChavan4 <102905814+LataChavan4@users.noreply.github.com>
2023-10-19 23:56:57 +0530

    Create Adding_deleting_data_in_table.sql

---
+++ Adding_deleting_data_in_table.sql
@@ -0,0 +1,9 @@
+--Adding records in table
+insert into amazon_orders values(1,'2022-10-01','Baby Milk',30.5,'UPI');
+insert into amazon_orders values(2,'2022-10-02','Baby Powder',130,'Credit Card');
+
+-- below comment will delete only data from given table keeping its structure intact
+delete from table_name;
+
+--Drop will delete entire table 
+drop table table_name;
